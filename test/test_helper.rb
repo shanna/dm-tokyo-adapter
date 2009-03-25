cd2dde9fe9dab3abda68b2603bf144ad253b1b40
Shane Hanna <shane.hanna@gmail.com>
2009-03-26 01:05:00 +1100

    Initial commit to dm-tokyo-cabinet-adapter.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'dm_tokyo_cabinet_adapter'
+
+class Test::Unit::TestCase
+end
